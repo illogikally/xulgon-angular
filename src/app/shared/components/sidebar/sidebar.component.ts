33c20c9897682f4c1c687dc4e5313914a8001a93
xael <xael5813@gmail.com>
2022-04-01 17:33:41 +0700

    feat: sidebar on window resize

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, NgZone, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { fromEvent, merge, of, Subject } from 'rxjs';
+import { mergeAll, switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,15 +10,19 @@ import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@ang
 export class SidebarComponent implements OnInit {
 
   @Input() toggleButtonTop = '10px';
+  @Input() attach$ = new Subject<any>();
+  @Input() detach$ = new Subject<any>();
   constructor(
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private changeDetector: ChangeDetectorRef
   ) { }
 
-  @ViewChild('sidebar', {static: true}) sidebar!: ElementRef;
-  @ViewChild('sidebarInner', {static: true}) sidebarInner!: ElementRef;
-  @ViewChild('toggleButton', {static: true}) toggleButton!: ElementRef;
+  @ViewChild('sidebar', { static: true }) sidebar!: ElementRef;
+  @ViewChild('sidebarInner', { static: true }) sidebarInner!: ElementRef;
+  @ViewChild('toggleButton', { static: true }) toggleButton!: ElementRef;
 
   ngOnInit(): void {
+    this.configureOnWindowResize();
   }
 
   toggleSidebar() {
@@ -30,6 +36,35 @@ export class SidebarComponent implements OnInit {
     else {
       elems.forEach(elem => this.renderer.addClass(elem, 'hide'));
     }
+  }
+
+  configureOnWindowResize() {
+    const onWindowResize$ = fromEvent(window, 'resize').pipe(takeUntil(this.detach$));
+    let prevWindowWidth = 0;
+    const breakPoint = 750;
+    const transitionTime = 400;
+    merge(this.attach$, of(null)).pipe(
+      switchMap(() => onWindowResize$)
+    ).subscribe(() => {
+      const isHide = this.sidebarInner.nativeElement.classList.contains('hide');
+      if (
+        window.innerWidth < breakPoint &&
+        prevWindowWidth >= breakPoint
+      ) {
+        !isHide && this.toggleSidebar();
+        setTimeout(() => {
+          this.renderer.setStyle(this.sidebar.nativeElement, 'position', 'absolute');
+        }, transitionTime);
+      }
+      else if (
+        window.innerWidth > breakPoint &&
+        prevWindowWidth <= breakPoint
+      ) {
+        this.renderer.removeStyle(this.sidebar.nativeElement, 'position');
+        isHide && this.toggleSidebar();
+      }
+      prevWindowWidth = window.innerWidth;
+    });
   }
 
   get toggleButtonPosition() {
